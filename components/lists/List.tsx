d7535a0857bfdec118b0c10d3d3c6c8c44923cf9
coffmanjrp <coffmanjrp@gmail.com>
2021-03-28 14:07:36 -0700

    Add useState hook

--- components/lists/List.tsx
+++ components/lists/List.tsx
@@ -1,33 +1,33 @@
+import { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { pokemonsListQuery } from 'utils/queries';
 import { Loading } from 'components/layouts';
 import { ListItem } from './';
 
 const List = () => {
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(30);
   const { data, loading, error } = useQuery(pokemonsListQuery, {
     variables: {
-      offset: 0,
-      limit: 151,
+      offset,
+      limit,
     },
   });
 
-  if (error) {
-    return <h2 className="text-xl font-bold text-center">Error</h2>;
-  }
-
-  if (loading) {
-    return <Loading />;
-  }
-
   return (
     <>
-      <div className="flex gap-5 flex-wrap justify-center items-center mx-auto max-w-screen-xl">
-        {data &&
-          data.pokemons.map(
+      {error ? (
+        <h2 className="text-xl font-bold text-center">Error</h2>
+      ) : loading ? (
+        <Loading />
+      ) : (
+        <div className="flex gap-5 flex-wrap justify-center items-center mx-auto max-w-screen-xl">
+          {data.pokemons.map(
             (pokemon) =>
               pokemon !== null && <ListItem key={pokemon.id} {...pokemon} />
           )}
-      </div>
+        </div>
+      )}
     </>
   );
 };
