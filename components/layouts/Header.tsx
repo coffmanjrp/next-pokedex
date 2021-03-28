8e53da9b03ce5d837781fab56ae5956efdabfafe
coffmanjrp <coffmanjrp@gmail.com>
2021-03-28 13:48:07 -0700

    Fix tag element

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,9 +1,9 @@
 const Header = ({ title }) => {
   return (
     <>
-      <div className="mb-10 w-full py-5 px-2">
+      <header className="mb-10 w-full py-5 px-2">
         <h1 className="text-4xl font-bold text-center">{title}</h1>
-      </div>
+      </header>
     </>
   );
 };
